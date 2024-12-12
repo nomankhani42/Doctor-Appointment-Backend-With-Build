1a5d420fc6375421132407a27ce011843186c80c
Noman Khan <khaninonan@gmail.com>
2024-12-12 06:09:12 -0800

    added inital dist

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ import { AppointmentRouter } from './Routes/appointment.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 
 
@@ -16,13 +21,14 @@ dotenv.config();
 const app=express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'./dist')))
 
 
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://webdevelopernoman:1Cz4gJtEKlCxQ7Pc@cluster0.hcxeo.mongodb.net/');
+  await mongoose.connect(process.env.mongo_Url);
   console.log('DB Connected')
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
@@ -34,6 +40,9 @@ async function main() {
 app.use('/api/v1',userRouter)
 app.use('/api/v1/doctor',DoctorRouter);
 app.use('/api/v1/appointment',AppointmentRouter);
+app.use('*',(req,res)=>{
+  res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+})
 
 
 
